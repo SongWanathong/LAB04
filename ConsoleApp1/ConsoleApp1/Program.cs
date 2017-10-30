a110a769c501d758808b857c2ad504b59b94b2b2
song <song@DESKTOP-C6V3TNA>
2017-10-30 20:46:40 +0700

    go to and loop

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,26 +1,51 @@
-﻿
-using System;
-public class GotoTest
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Line 1");
-        goto line4;
-    line2:   
-        Console.WriteLine("Line 2");
-        goto line9;
-        Console.WriteLine("Line 3");
-    line4:
-        Console.WriteLine("Line 4");
-        Console.WriteLine("Line 5");
-        goto line2;
-        Console.WriteLine("Line 6");
-       
-        Console.WriteLine("Line 7");
-        Console.WriteLine("Line 8");
-    line9:
-        Console.WriteLine("Line 9");
-        
+﻿using System;
 
+namespace ConsoleApp2
+{
+    public class GotoWithLoop
+    {
+        static void Main()
+        {
+            int x = 200, y = 4;
+            int count = 0;
+            string[,] array = new string[x, y];
+
+            // Initialize the array:
+            for (int i = 0; i < x; i++)
+
+                for (int j = 0; j < y; j++)
+                    array[i, j] = (++count).ToString();
+
+            // Read input:
+            Console.Write("Enter the number to search for: ");
+
+            // Input a string:
+            string myNumber = Console.ReadLine();
+
+            // Search:
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if (array[i, j].Equals(myNumber))
+                    {
+                        goto Found;
+                    }
+                }
+            }
+
+            Console.WriteLine("The number {0} was not found.", myNumber);
+            goto Finish;
+
+        Found:
+            Console.WriteLine("The number {0} is found.", myNumber);
+
+        Finish:
+            Console.WriteLine("End of search.");
+
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
     }
 }
